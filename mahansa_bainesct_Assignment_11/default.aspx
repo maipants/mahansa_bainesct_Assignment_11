eb9623b251b260d04ccc0654661cf84671703a1d
maipants <tkrkh43@gmail.com>
2024-04-04 11:59:58 -0400

    Yeehaw

--- mahansa_bainesct_Assignment_11/default.aspx
+++ mahansa_bainesct_Assignment_11/default.aspx
@@ -9,6 +9,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <label id="lblTotalDays" Text="Total Days"></label>
         </div>
     </form>
 </body>
--- mahansa_bainesct_Assignment_11/default.aspx.cs
+++ mahansa_bainesct_Assignment_11/default.aspx.cs
@@ -13,5 +13,20 @@ namespace mahansa_bainesct_Assignment_11
         {
 
         }
+
+        protected void cmd_ClickOK(object sender, EventArgs e)
+        {
+            // Compute the total days in the reservation,
+            // Assume $200 per night,
+            // apply tax, 15%, and diplay in lblTotalCost
+            int days;
+            var checkInDate = calCheckInDate.SelectedDate;
+            var checkOutDate = calCheckOutDate.SelectedDate;
+
+            days = (checkOutDate - checkInDate).Days;
+            lblTotalDays.Text = days.ToString() + " Days";
+
+            lblTotalCost.Text = "$" + ((days * 200) * 1.15).ToString();
+        }
     }
 }
